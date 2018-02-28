09ac4203dab277ad4f49404f45477371e062d2b4
Олег Моисеев <oleg-98@list.ru>
2018-02-28 14:12:11 +0300

    Added PLU-decomposition with selection of an element throughout the matrix

--- PLU.py
+++ PLU.py
@@ -13,10 +13,18 @@ def matrix_multiplication(a, b):
     return mat
 
 
-def matrix_determinant(u):
-    det = 1.
-    for i in range(3):
-        det *= u[i, i]
+def matrix_determinant(p, l, u):
+    ml = 1.
+    mu = 1.
+    rows, cols = p.shape
+
+    for i in range(rows):
+        ml *= l[i, i]
+        mu *= u[i, i]
+
+    det = ml * mu
+    if rows % 2:
+        det *= -1
     return det
 
 
@@ -26,7 +34,14 @@ def swap_rows(m, s, f):
     m[s, :] = tmp
 
 
-def plu(a):
+def swap_cols(m, s, f):
+    tmp = np.copy(m[:, f])
+    m[:, f] = np.copy(m[:, s])
+    m[:, s] = tmp
+
+
+def plu(a_orig):
+    a = np.copy(a_orig)
     rows, cols = a.shape
     p = np.eye(rows, cols)
     for k in range(rows):
@@ -36,7 +51,7 @@ def plu(a):
                 pivot_value = abs(a[i, k])
                 row_with_max_elem = i
 
-        if pivot_value == 0:
+        if pivot_value < 10e-16:
             raise Exception("Degenerate matrix")
 
         swap_rows(a, k, row_with_max_elem)
@@ -47,29 +62,103 @@ def plu(a):
             for j in range(k + 1, rows):
                 a[i, j] -= a[i, k] * a[k, j]
 
-    l = np.zeros((rows, cols))
-    u = np.zeros((rows, cols))
+    lower = np.zeros((rows, cols))
+    upper = np.zeros((rows, cols))
 
     for i in range(rows):
-        l[i, i] = 1.
+        lower[i, i] = 1.
         for j in range(cols):
             if i > j:
-                l[i, j] = a[i, j]
+                lower[i, j] = a[i, j]
             else:
-                u[i, j] = a[i, j]
+                upper[i, j] = a[i, j]
 
-    return p, l, u
-
-
-def check_plu(p, l, u):
-    return matrix_multiplication(p, matrix_multiplication(l, u))
+    return p, lower, upper
 
 
-A = np.array([[2., 7., -6.],
+def check_plu(a, p, l, u):
+    print "Original matrix:"
+    print a
+    print "P*L*U:"
+    plu_mat = matrix_multiplication(p, matrix_multiplication(l, u))
+    print plu_mat
+    print "Matrices are equivalent:", np.array_equal(a, plu_mat)
+
+
+def plu_full(a_orig):
+    a = np.copy(a_orig)
+    rows, cols = a.shape
+    p = np.eye(rows, cols)
+    for k in range(rows):
+        pivot_value = 0
+
+        for i in range(k, rows):
+            for j in range(k, cols):
+                if abs(a[i, j]) > pivot_value:
+                    pivot_value = abs(a[i, j])
+                    row_with_max_elem = i
+                    col_with_max_elem = j
+
+        if pivot_value < 10e-16:
+            raise Exception("Degenerate matrix")
+        print a
+        print "_________________________________t"
+        swap_rows(a, k, row_with_max_elem)
+        swap_cols(a, k, col_with_max_elem)
+
+        swap_rows(p, k, row_with_max_elem)
+        swap_cols(p, k, col_with_max_elem)
+
+        for i in range(k + 1, rows):
+            a[i, k] /= a[k, k]
+            for j in range(k + 1, cols):
+                a[i, j] -= a[i, k] * a[k, j]
+
+    lower = np.zeros((rows, cols))
+    upper = np.zeros((rows, cols))
+
+    for i in range(rows):
+        lower[i, i] = 1.
+        for j in range(cols):
+            if i > j:
+                lower[i, j] = a[i, j]
+            else:
+                upper[i, j] = a[i, j]
+
+    return p, lower, upper
+
+
+A = np.array([[2., 7., 6.],
               [8., 2., 1.],
               [7., 4., 2.]])
 
-P, L, U = plu(A)
+B = np.array([[2., 7., -6., 5., -7.],
+              [8., 2., 1., -56., 2.],
+              [7., 4., 2., 9., 3.],
+              [-1., 4., -6., -9., 3.],
+              [7., 4., 2., 9., 45.]])
 
-print check_plu(P, L, U)
+C = np.array([[2., 7.],
+              [8., 2.]])
 
+D = np.array([[2., 7., -6., 5., -7., 4.],
+              [8., 2., 1., -56., 2., 9.],
+              [7., 4., 2., 9., 3., 2.],
+              [-1., 4., -6., -9., 3., -4.],
+              [7., 4., 2., 9., 45., -7],
+              [-12., 34., -26., -19., 23., -54.]])
+try:
+
+    P, L, U = plu_full(B)
+
+    print "Matrix P:"
+    print P
+    print "Matrix L:"
+    print L
+    print "Matrix U:"
+    print U
+
+    check_plu(B, P, L, U)
+    print "Det:", matrix_determinant(P, L, U)
+except Exception as e:
+    print e
