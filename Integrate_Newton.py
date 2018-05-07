25f8e753a1fe918072c0045df1281c779290ce42
Олег Моисеев <oleg-98@list.ru>
2018-05-07 10:38:51 +0300

    Started 4th task

---
+++ Integrate_Newton.py
@@ -0,0 +1,30 @@
+import SLE
+
+x1 = a
+x2 = (a + b) / 2
+x3 = b
+p(x) = 1 / ((x - a)^alpha * (x - b)^beta))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# f(x) = 3.7 * cos(1.5 * x) * exp(−4 * x / 3) + 2.4 * sin(4.5 * x) * exp(2 * x / 3) + 4
+# a = 1.8
+# b = 2.3
+# alpha = 0
+# beta = 3 / 5
+# integrate: (f(x)/((x - a)^alpha * (x - b)^beta)) dx from a to b
+
